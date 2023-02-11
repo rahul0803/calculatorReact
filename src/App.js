da6e0611bd2c5b284432b029107d790c56ed64a9
Rahul <royrahulrk12@gmail.com>
2023-02-11 22:24:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,140 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [result, setResult] = useState("");
+  const [msg, setMsg] = useState("");
+  const [message, successMessage] = useState("");
+
+  const addNum = () => {
+    if (!number || !number2) {
+      setMsg("Error: Input fields can't be empty");
+      successMessage("");
+      setResult("");
+      document.getElementById("result").style.display = "none";
+    }
+    if (number && number2) {
+      if (isNaN(number) || isNaN(number2)) {
+        setNumber("");
+        setNumber2("");
+        successMessage("");
+        setMsg("");
+        document.getElementById("result").style.display = "none";
+      }
+    }
+    if (number && number2) {
+      if (!isNaN(number) && !isNaN(number2)) {
+        successMessage("Success: Your result is shown above!");
+        setMsg("");
+        setResult(+number + +number2);
+        document.getElementById("result").style.display = "block";
+      }
+    }
+  }
+
+  const subNum = () => {
+    if (!number || !number2) {
+      setMsg("Error: Input fields can't be empty");
+      successMessage("");
+      setResult("");
+      document.getElementById("result").style.display = "none";
+    }
+    if (number && number2) {
+      if (isNaN(number) || isNaN(number2)) {
+        setNumber("");
+        setNumber2("");
+        successMessage("");
+        setMsg("");
+        document.getElementById("result").style.display = "none";
+      }
+    }
+    if (number && number2) {
+      if (!isNaN(number) && !isNaN(number2)) {
+        successMessage("Success: Your result is shown above!");
+        setMsg("");
+        setResult(+number - +number2);
+        document.getElementById("result").style.display = "block";
+      }
+    }
+  }
+
+  const mulNum = () => {
+    if (!number || !number2) {
+      setMsg("Error: Input fields can't be empty");
+      successMessage("");
+      setResult("");
+      document.getElementById("result").style.display = "none";
+    }
+    if (number && number2) {
+      if (isNaN(number) || isNaN(number2)) {
+        setNumber("");
+        setNumber2("");
+        successMessage("");
+        setMsg("");
+        document.getElementById("result").style.display = "none";
+      }
+    }
+    if (number && number2) {
+      if (!isNaN(number) && !isNaN(number2)) {
+        successMessage("Success: Your result is shown above!");
+        setMsg("");
+        setResult(+number * +number2);
+        document.getElementById("result").style.display = "block";
+      }
+    }
+  }
+
+  const divNum = () => {
+    if (!number || !number2) {
+      setMsg("Error: Input fields can't be empty");
+      successMessage("");
+      setResult("");
+      document.getElementById("result").style.display = "none";
+    }
+    if (number && number2) {
+      if (isNaN(number) || isNaN(number2)) {
+        setNumber("");
+        setNumber2("");
+        successMessage("");
+        setMsg("");
+        document.getElementById("result").style.display = "none";
+      }
+    }
+    if (number && number2) {
+      if (!isNaN(number) && !isNaN(number2)) {
+        successMessage("Success: Your result is shown above!");
+        setMsg("");
+        setResult(+number / +number2);
+        document.getElementById("result").style.display = "block";
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="heading">React Calculator</h1>
+
+        <input type="text" placeholder="Num 1" className="box1" value={number} onChange={(e) => {
+          setNumber(e.target.value);
+        }}></input>
+        <input type="text" placeholder="Num 2" className="box2" value={number2} onChange={(e) => {
+          setNumber2(e.target.value);
+        }}></input>
+
+        <button id='add' onClick={addNum}>+</button>
+        <button id='sub' onClick={subNum}>-</button>
+        <button id='mul' onClick={mulNum}>*</button>
+        <button id='div' onClick={divNum}>/</button>
+
+        <p id='result'>Result={result}</p>
+        <p id='outputMsg'>{msg}</p>
+        <p id='successMsg'>{message}</p>
+
+      </div>
     </div>
   );
 }
